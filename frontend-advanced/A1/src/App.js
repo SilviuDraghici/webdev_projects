cd2ce72999b8136319e555655006b1ff3cd188d4
Silviu Draghici <silviu.dan.draghici@gmail.com>
2022-03-05 22:49:57 -0500

    A1: changing the color of the players *wip*

--- frontend-advanced/A1/src/App.js
+++ frontend-advanced/A1/src/App.js
@@ -12,10 +12,10 @@ function App() {
       <Header />
 
       <div className="row justify-content-evenly mt-4">
-        <PlayerCard name="Player 1" />
-        <PlayerCard name="Player 2" />
-        <PlayerCard name="Player 3" />
-        <PlayerCard name="Player 4" />
+        <PlayerCard name="Player 1" playerNum={1} />
+        <PlayerCard name="Player 2" playerNum={2}/>
+        <PlayerCard name="Player 3" playerNum={3}/>
+        <PlayerCard name="Player 4" playerNum={4}/>
       </div>
     </div>
   );
--- frontend-advanced/A1/src/components/PlayerCard.js
+++ frontend-advanced/A1/src/components/PlayerCard.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 
-const PlayerCard = ({ name }) => {
+const PlayerCard = ({ name, playerNum }) => {
+    const [colorAssignment, updateColorAssignment] = useState(["white", "white", "white", "white"]);
+
+    const setColor = (color) => {
+        //console.log(`color selected: ${color}`);
+        updateColorAssignment(assignment => {
+            assignment[playerNum - 1] = color;
+            console.log(`new color assignment: ${assignment}`);
+            return assignment;
+        });
+    }
+
     return (
-        <div className=" card col-12 col-md-5 mb-4 "
-            style={PlayerCardStyle}>
+        <div className="card col-12 col-md-5 mb-4 "
+            style={{ ...PlayerCardStyle, ...colors[colorAssignment[playerNum - 1]] }}>
             <div className="card-header text-center"
                 style={headerStyle}>
                 <header>
@@ -13,23 +26,19 @@ const PlayerCard = ({ name }) => {
             <div className="card-body">
                 <DropdownButton id="dropdown-basic-button" title="Select Color"
                     variant="dark"
-                    onSelect={colorSelect}>
+                    onSelect={setColor}>
                     <Dropdown.Item eventKey="red"
-                        style={red}>red</Dropdown.Item>
+                        style={colors["red"]}>red</Dropdown.Item>
                     <Dropdown.Item eventKey="green"
-                        style={green}>green</Dropdown.Item>
+                        style={colors['green']}>green</Dropdown.Item>
                     <Dropdown.Item eventKey="blue"
-                        style={blue}>blue</Dropdown.Item>
+                        style={colors['blue']}>blue</Dropdown.Item>
                     <Dropdown.Item eventKey="yellow"
-                        style={yellow}>yellow</Dropdown.Item>
+                        style={colors['yellow']}>yellow</Dropdown.Item>
                 </DropdownButton>
             </div>
         </div >
     )
-}
-
-const colorSelect = (key) => {
-    console.log(`color selected: ${key}`);
 }
 
 const PlayerCardStyle = {
@@ -42,24 +51,26 @@ const headerStyle = {
     alignItems: 'center'
 }
 
-
-const red = {
-    backgroundColor: 'red',
-    color: 'white'
-}
-
-const green = {
-    backgroundColor: 'green',
-    color: 'white'
-}
-
-const blue = {
-    backgroundColor: 'blue',
-    color: 'white'
-}
-
-const yellow = {
-    backgroundColor: 'yellow'
+const colors = {
+    white: {
+        backgroundColor: 'white',
+        color: 'black'
+    },
+    red: {
+        backgroundColor: 'red',
+        color: 'white'
+    },
+    green: {
+        backgroundColor: 'green',
+        color: 'white'
+    },
+    blue: {
+        backgroundColor: 'blue',
+        color: 'white'
+    },
+    yellow: {
+        backgroundColor: 'yellow'
+    }
 }
 
 export default PlayerCard
