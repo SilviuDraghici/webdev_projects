000861f4cb3c1d88a370b9ee7923924aa32b12d1
Silviu Draghici <silviu.dan.draghici@gmail.com>
2022-03-13 20:29:25 -0400

    put onSnaphsot into useEffect.

--- backend-basics/A1/src/App.js
+++ backend-basics/A1/src/App.js
@@ -1,13 +1,14 @@
 import "./styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { collection, doc, onSnapshot} from 'firebase/firestore';
+import { collection, doc, onSnapshot } from 'firebase/firestore';
 
 import GameLobby from "./components/GameLobby";
 import About from "./components/About";
+
 import AssignedColorsContext from './components/AssignedColorsContext';
 
 import firestore from "./firebase";
@@ -15,15 +16,16 @@ import firestore from "./firebase";
 function App() {
   const [assignedColors, updateAssignedColors] = useState(["default", "default", "default", "default"]);
 
-  console.log(firestore);
+  
   const cols = collection(firestore, "colors");
-  console.log(cols);
-
-  onSnapshot(cols, (snapshot) => {
-    snapshot.forEach((doc) => {
-      console.log(doc.data());
-    })
-  });
+  
+  useEffect(() => {
+    onSnapshot(cols, (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.data());
+      })
+    });
+  }, []);
 
   const assignColor = (color, playerNum) => {
     //console.log(`color selected: ${color}`);
