147f9392c27314d3f2e23f60c1935e1c9469624e
Silviu Draghici <silviu.dan.draghici@gmail.com>
2022-03-21 20:29:55 -0400

    lint b.s.

--- backend-basics/A1-cloudFunctions/functions/index.js
+++ backend-basics/A1-cloudFunctions/functions/index.js
@@ -1,19 +1,18 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.selectColor = functions.https.onCall((data, context)=> {
-	if(!context.auth){
-		console.log(`Unauthenticated call made!`);
-		throw new functions.https.HttpsError(
-			'unauthenticated',
-			'Must be signed in to change player status!'
-		);
-	}
-	const player = admin.firestore().collection('players').doc(context.auth.uid);
-	console.log(`updating doc (${context.auth.uid}) with {color: ${data.color}}`);
-	return player.set(data, { merge: true });
+  if (!context.auth) {
+    console.log("Unauthenticated call made!");
+    throw new functions.https.HttpsError(
+        "unauthenticated",
+        "Must be signed in to change player status!"
+    );
+  }
+  const player = admin.firestore().collection("players").doc(context.auth.uid);
+  console.log(`updating doc (${context.auth.uid}) with {color: ${data.color}}`);
+  return player.set(data, {merge: true});
 });
